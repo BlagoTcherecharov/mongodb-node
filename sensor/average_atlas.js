c92b298d3293fec97fcfb47e552e41ad6c4539d8
BlagoTcherecharov <117592898+BlagoTcherecharov@users.noreply.github.com>
2024-11-13 15:13:22 +0200

    Create average_atlas.js

---
+++ sensor/average_atlas.js
@@ -0,0 +1,67 @@
+const { MongoClient } = require('mongodb');
+
+const url = "mongodb+srv://blago:PASSWORD@cluster0.ehhe5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbName = 'my-sensors';
+const collectionName = 'data';
+const options = {
+    serverSelectionTimeoutMS: 3000,
+    connectTimeoutMS: 3000,
+    socketTimeoutMS: 3000
+};
+const sensorType = 'humidity';
+
+async function startQuery() {
+    let client;
+    console.time("Connect time");
+
+    try {
+        client = await MongoClient.connect(url, options);
+    } catch (error) {
+        console.log(`Timeout: \n${error.message}`);
+        return;
+    } finally {
+        console.timeEnd("Connect time");
+    }
+
+    const collection = client.db(dbName).collection(collectionName);
+    console.time("Aggregate time");
+
+    try {
+        const results = await collection
+            .aggregate(
+                [
+                    {
+                      '$match': {
+                        'timestamp': {
+                          '$lt': new Date('Sun, 20 Jun 2021 00:00:00 GMT')
+                        }
+                      }
+                    }, {
+                      '$group': {
+                        '_id': '$type', 
+                        'avg': {
+                          '$avg': '$value'
+                        }
+                      }
+                    }
+                  ]
+            ).toArray();
+
+            results.forEach(result => {
+                const type = result._id;
+                const units = type === 'temperature' ? '\u00B0C' : '%';
+                console.log(`Average ${type}: ${result.avg.toFixed(2)}${units}`);
+            });
+    } 
+    catch (error) 
+    {
+        console.log(error.message);
+    } 
+    finally 
+    {
+        client.close();
+        console.timeEnd("Aggregate time");
+    }
+}
+
+startQuery();
